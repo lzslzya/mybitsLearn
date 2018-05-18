0d7ebbef45e2d32cf76c2c829035da2a8f8c20a5
lizhongyuan15 <lizhongyuan9@jd.com>
2018-05-18 20:56:57 +0800

    char01

--- src/test/java/App.java
+++ src/test/java/App.java
@@ -1,3 +1,4 @@
+import com.yiibai.mybatis.dao.IUser;
 import com.yiibai.mybatis.models.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -14,6 +15,7 @@ public class App {
         try {
             reader = Resources.getResourceAsReader("mybatis-config.xml");
             sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            sqlSessionFactory.getConfiguration().addMapper(IUser.class);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -26,7 +28,12 @@ public class App {
      * @param args
      */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        // char00 使用xml配置mapper
+        //char00();
+        char01();
+    }
+
+    private static void char00(){
         SqlSession session = sqlSessionFactory.openSession();
         try {
             User user = (User) session.selectOne(
@@ -36,6 +43,19 @@ public class App {
                 System.out.println(userInfo);
             }
         } finally {
+            session.close();
+        }
+    }
+
+    private static void char01(){
+        SqlSession session = sqlSessionFactory.openSession();
+        try{
+            IUser iUser = session.getMapper(IUser.class);
+            User user = iUser.getUserByID(1);
+            System.out.println("名字："+user.getName());
+            System.out.println("所属部门："+user.getDept());
+            System.out.println("主页："+user.getWebsite());
+        }finally {
             session.close();
         }
     }
